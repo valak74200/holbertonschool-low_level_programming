e2dca99f101ae1f0616dcef06f4141b39a0d1233
valak74200 <clmentlv@outlook.fr>
2024-10-23 08:39:35 +0000

    is a program that prints all possible combinations of two two-digit numbers.

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -12,19 +12,16 @@ int main(void)
 	{
 		for (j = i + 1; j <= 99; j++)
 		{
-			if (i < j)
-			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
 				putchar(' ');
 				putchar((j / 10) + '0');
 				putchar((j % 10) + '0');
+
 				if (i == 98 && j == 99)
-				{
+
 					putchar(',');
 					putchar(' ');
-				}
-			}
 		}
 	}
 	putchar('\n');
