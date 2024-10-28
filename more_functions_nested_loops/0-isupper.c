4c35e3f7c3a5ab027e1290f137d39d406df77836
valak74200 <clmentlv@outlook.fr>
2024-10-28 09:15:38 +0000

    is a function that checks for uppercase character.

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * _isupper - checks for uppercase character
  * @c: the character to be checked
  *
@@ -9,6 +9,7 @@
  */
 
 int _isupper(int c)
+
 {
 	return (c >= 'A' && c <= 'Z');
 }
