c78a4b0d27c12ccf2c982104480d4941ae69d61d
valak74200 <clmentlv@outlook.fr>
2024-10-28 09:57:36 +0000

    is a function that prints the numbers, from 0 to 9, followed by a new line.

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -5,7 +5,7 @@
  * except 2 and 4, followed by a new line
  */
 
-void print_miost_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
@@ -13,8 +13,8 @@ void print_miost_numbers(void)
 	{
 		if (i != 2 && i != 4)
 		{
-			_putchar(i + '0')
+			_putchar(i + '0');
 		}
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
