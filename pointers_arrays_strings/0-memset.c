d4a6e4919b67f677300f12a74d9e360d8a21c28f
valak74200 <clmentlv@outlook.fr>
2024-11-05 14:59:23 +0100

    Advanced task

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -10,31 +10,31 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 
-//Cette ligne déclare la fonction _memset qui prend trois paramètres :
-//s : un pointeur vers la zone mémoire à remplir
-//b : le caractère à copier dans la mémoire
-//n : le nombre de fois que b doit être copié
-//La fonction retourne un pointeur de type char*.
+// Cette ligne déclare la fonction _memset qui prend trois paramètres :
+// s : un pointeur vers la zone mémoire à remplir
+// b : le caractère à copier dans la mémoire
+// n : le nombre de fois que b doit être copié
+// La fonction retourne un pointeur de type char*.
 	
 	unsigned int i;
 
-//Déclare une variable non signée i qui sera utilisée comme compteur dans la boucle.
+// Déclare une variable non signée i qui sera utilisée comme compteur dans la boucle.
 
 	for (i = 0; i < n; i++)
 	{
 
-//Commence une boucle for qui s'exécutera n fois.
+// Commence une boucle for qui s'exécutera n fois.
 
 		s[i] = b;
 
-//À chaque itération de la boucle, cette ligne assigne le caractère b à la position i dans la zone mémoire pointée par s.
+// À chaque itération de la boucle, cette ligne assigne le caractère b à la position i dans la zone mémoire pointée par s.
 
 	}
 
 	return (s);
 
-//Après avoir rempli la mémoire, la fonction retourne le pointeur s vers la zone mémoire modifiée.
+// Après avoir rempli la mémoire, la fonction retourne le pointeur s vers la zone mémoire modifiée.
 
 }
 
-//Cette fonction est utile pour initialiser ou réinitialiser une zone de mémoire avec une valeur spécifique. Par exemple, elle peut être utilisée pour effacer un buffer en le remplissant de zéros, ou pour initialiser un tableau avec une valeur par défaut.
+// Cette fonction est utile pour initialiser ou réinitialiser une zone de mémoire avec une valeur spécifique. Par exemple, elle peut être utilisée pour effacer un buffer en le remplissant de zéros, ou pour initialiser un tableau avec une valeur par défaut.
