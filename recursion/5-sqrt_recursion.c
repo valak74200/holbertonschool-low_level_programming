f6386183286ba98490f34eaa0bd61442e28aec62
valak74200 <clmentlv@outlook.fr>
2024-11-05 14:09:46 +0100

    Write a function that returns 1 if the input integer is a prime number, otherwise return 0.

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-int actual_sqrt_recursion(int n, int i)
+int actual_sqrt_recursion(int n, int i);
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
