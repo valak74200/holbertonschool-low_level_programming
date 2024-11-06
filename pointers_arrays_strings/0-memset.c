6b5f2660204987573535daf2ba5bd9cc9eedc3fd
valak74200 <clmentlv@outlook.fr>
2024-11-06 15:13:53 +0100

    Add Comments

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -8,12 +8,28 @@
  * @n: number of times to copy b
  */
 char *_memset(char *s, char b, unsigned int n)
+
+// C'est la déclaration de la fonction _memset. Elle prend trois paramètres :
+// s : un pointeur vers la zone mémoire à remplir
+// b : le caractère à copier dans la mémoire
+// n : le nombre de fois que b doit être copié
+
 {
 	unsigned int i;
+
+// Déclaration de la variable i de type unsigned int qui sera utilisée comme compteur dans la boucle.
 
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
+
+// Cette boucle for itère n fois. À chaque itération, elle copie le caractère b dans la position i de la zone mémoire pointée par s.
+
 	return (s);
+
+// La fonction retourne le pointeur s, qui pointe maintenant vers la zone mémoire modifiée.
+
 }
+
+// En résumé, cette fonction est une implémentation personnalisée de la fonction standard memset. Elle remplit les n premiers octets de la zone mémoire pointée par s avec le caractère b. Cette fonction est utile pour initialiser ou réinitialiser une zone mémoire avec une valeur spécifique.
